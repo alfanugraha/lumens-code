220a823d8c67ce0409b70a7daf05747618289db4
lumens.icraf <lumens.icraf@7aafdbc6-8c9f-ad5e-3ac2-0cc931368817>
2014-10-09 07:31:30 +0000

    added by Dony, PUR Reconciled by reference map with auto join feature (PUR_ext_join_att_table.rsx)

--- PUR_ext_join_att_table.R
+++ PUR_ext_join_att_table.R
@@ -4,11 +4,12 @@
 ##ref=file
 ##loc=string
 ##PUR_dbfinal=output table
-##report
+##merged_shp=output vector 
 
 
 library(sp)
 library(rgdal)
+library(foreign)
 
 wd <- "C:/LUMENS_kal/1_PUR/3_Kutim"
 input_poly <- "C:/LUMENS_kal/1_PUR/3_Kutim/kutim_output.shp"
@@ -17,17 +18,15 @@ setwd (wd)
 
 # PREPARE REFERENCE DATA
 input_poly<- substr(basename(input_poly), 1, nchar(basename(input_poly)) - 4)
-sa<-readOGR(dsn=wd,input_poly)
+#sa<-readOGR(dsn=wd,input_poly)
 
 
 #read and modify PUR table
 input_table <- read.dbf(input_table)
-shp.tab.input<-as.data.frame(input_table[1]) 
+shp.tab.input<-as.data.frame(input_table) 
 colnames(shp.tab.input)<-c("DN","reconciled","area")
-Merged_shp<-merge(sa, shp.tab.input, by="DN")
+merged_shp<-merge(sa, shp.tab.input, by="DN")
 
 
-
-
-
-
+#write joined shapefile
+writeOGR(merged_shp, wd, "PUR_reconciled_planning_unit",driver="ESRI Shapefile")
