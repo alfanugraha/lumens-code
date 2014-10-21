e382f358c947ea64cc84ff639e3eed94d84f453a
lumens.icraf <lumens.icraf@7aafdbc6-8c9f-ad5e-3ac2-0cc931368817>
2014-10-21 09:25:51 +0000

    QuES-B minor update, without ratify func., NA data handling, add some variables into .ldbase QuES-B focal area integration-segregation analysis update, working time series segregation-integration analysis, zonal statistics, and combine class metrics statistics.
    further improvement: import .ldbase from Pre-QuES~elaborate QuES-B with landcover change database

--- trunk/QuES-B focal area integration-segregation analysis.R
+++ trunk/QuES-B focal area integration-segregation analysis.R
@@ -23,7 +23,10 @@ foc.area.init<-habitat
 lu.init<-lu1
 mwfile.init<-mwfile
 plotbio.init<-plotbio
-remove(sumtab1,teci_zstat,habitat,lu1,mwfile,plotbio)
+foc.area.stats.init<-foc.area.stats
+year.init<-year
+AUC.init<-AUC2
+remove(sumtab1,teci_zstat,habitat,lu1,mwfile,plotbio,foc.area.stats, year,AUC2)
 
 #load initial database
 load(ldabase.final)
@@ -34,14 +37,17 @@ foc.area.final<-habitat
 lu.final<-lu1
 mwfile.final<-mwfile
 plotbio.final<-plotbio
-remove(sumtab1,teci_zstat,habitat,lu1,mwfile,plotbio)
+foc.area.stats.final<-foc.area.stats
+year.final<-year
+AUC.final<-AUC2
+remove(sumtab1,teci_zstat,habitat,lu1,mwfile,plotbio,foc.area.stats, year,AUC2)
 
 
 #Focal area decrement and increment
 chk_decrement<-foc.area.init>foc.area.final
 chk_increment<-foc.area.init<foc.area.final
-foc.area.decrement<-(foc.area.init-foc.area.final)*chk_decrement
-foc.area.increment<-(foc.area.final-foc.area.init)*chk_increment
+foc.area.decrement<-(foc.area.init-foc.area.final)*chk_decrement;#integration increase
+foc.area.increment<-(foc.area.final-foc.area.init)*chk_increment;#integration decrease
 
 
 #TECI increment and decrement except nodata
@@ -49,9 +55,12 @@ mwfile.init.chk<-mwfile.init
 mwfile.final.chk<-mwfile.final
 
 chk_teci_decrement<-mwfile.init.chk>mwfile.final.chk
+chk_teci_decrement <- reclassify(chk_teci_decrement, cbind(0,NA))
 chk_teci_increment<-mwfile.init.chk<mwfile.final.chk
+chk_teci_increment <- reclassify(chk_teci_increment, cbind(0,NA))
 mwfile.decrement<-(mwfile.init.chk-mwfile.final.chk)*chk_teci_decrement;#TECI value decrement
 mwfile.increment<-(mwfile.final.chk-mwfile.init.chk)*chk_teci_increment;#TECI value increment
+
 
 
 #TECI loss and gain in NA data
@@ -66,38 +75,41 @@ mwfile.gain.NA<-mwfile.final.chk*mwfile.init.NA;#TECI loss in NA area
 
 
 
-#FOCAL AREA SEGREGATION
-mwfile.gain.noNA <- reclassify(mwfile.gain, cbind(NA, 0))
-foc.area.seg<-mwfile.loss.NA+mwfile.gain.noNA
-
-
 #FOCAL AREA INTEGRATION
-mwfile.loss.noNA <- reclassify(mwfile.loss, cbind(NA, 0))
-foc.area.int<-mwfile.gain.NA+mwfile.loss.noNA
+mwfile.gain.NA <- reclassify(mwfile.gain.NA, cbind(0, NA))
+foc.area.int<-mosaic(mwfile.decrement, mwfile.gain.NA, fun="max")
+
+#FOCAL AREA SEGREGATION
+mwfile.loss.NA <- reclassify(mwfile.loss.NA, cbind(0, NA))
+foc.area.seg<-mosaic(mwfile.increment, mwfile.loss.NA, fun="max")
 
 
-
-
-NAmap.init<-mwfile.init.chk^0
 setwd("C:/QUES_B_DJB/gainloss_test/")
-writeRaster(chk_teci_loss,  filename="chk_teci_loss", format="GTiff", overwrite=TRUE)
-writeRaster(chk_teci_gain,  filename="chk_teci_gain", format="GTiff", overwrite=TRUE)
-writeRaster(mwfile.loss,  filename="mwfile_loss", format="GTiff", overwrite=TRUE)
-writeRaster(mwfile.gain,  filename="mwfile_gain", format="GTiff", overwrite=TRUE)
-writeRaster(foc.area.loss,  filename="foc_area_loss", format="GTiff", overwrite=TRUE)
-writeRaster(foc.area.gain,  filename="foc_area_gain", format="GTiff", overwrite=TRUE)
+
+writeRaster(mwfile.decrement,  filename="mwfile_decrement", format="GTiff", overwrite=TRUE)
+writeRaster(mwfile.increment,  filename="mwfile_increment", format="GTiff", overwrite=TRUE)
+
+writeRaster(mwfile.loss.NA,  filename="mwfile_loss", format="GTiff", overwrite=TRUE)
+writeRaster(mwfile.gain.NA,  filename="mwfile_gain", format="GTiff", overwrite=TRUE)
+
+writeRaster(foc.area.seg,  filename="foc_area_segregation", format="GTiff", overwrite=TRUE)
+writeRaster(foc.area.int,  filename="foc_area_integration", format="GTiff", overwrite=TRUE)
 
 writeRaster(foc.area.init,  filename="foc_area_init", format="GTiff", overwrite=TRUE)
 writeRaster(foc.area.final,  filename="foc_area_final", format="GTiff", overwrite=TRUE)
-foc.area.init
 
 
-chk_teci_loss<-mwfile.init.1>mwfile.final.1
-chk_teci_gain<-mwfile.init.1<mwfile.final.1
-mwfile.loss<-(mwfile.init-mwfile.final)*chk_teci_loss
-mwfile.gain<-(mwfile.final-mwfile.init)*chk_teci_gain
+#Combine patch statistics
+#teci_zstat<-cbind(teci_zstat.init,teci_zstat.final)
+#teci_zstat.init-teci_zstat.final
 
+#Combine class STATS
+foc.area.stats<-cbind(foc.area.stats.init,foc.area.stats.final)
+col.init<-paste('class.stats.',year.init, sep='')
+colnames(foc.area.stats)<-c(paste('class.stats.',year.init, sep=''),paste('class.stats.',year.final, sep=''))
 
+#zonal stat for habitat integration and segregation
+zstat.segregation<-ZonalStat(foc.area.seg, zone, FUN = "all") 
+zstat.integration<-ZonalStat(foc.area.int, zone, FUN = "all")
 
-
-
+#CONNECT with preques database
